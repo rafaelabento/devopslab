d657b6b76be615998d161f339ed688e5e08f431b
Rafaela Bento <rafaelasbento@yahoo.com.br>
2020-11-21 16:42:04 +0000

    alteracao app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run(debug=True)
