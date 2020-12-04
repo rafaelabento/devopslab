e6cd6a6838824afaded1939adf2f8682962e69c1
Rafaela Bento <rafaelasbento@yahoo.com.br>
2020-12-04 19:24:09 +0000

    Bug Adicionado

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
+
+@app.route('/bug')
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
