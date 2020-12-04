54495163cdc509e92dace6d2028b6c0e0b34ab27
Rafaela Bento <rafaelasbento@yahoo.com.br>
2020-12-04 19:32:58 +0000

    retirada do bug

--- app.py
+++ app.py
@@ -7,17 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
+    return "Laboratório Pipeline DevOps"                                                                                                                 
+        
 if __name__ == '__main__':
     app.run(debug=True)
