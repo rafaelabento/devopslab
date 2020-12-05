3eb398f46b926862447e461836bc502ec9580934
Rafaela Bento <rafaelasbento@yahoo.com.br>
2020-12-05 13:23:33 +0000

    ajuste app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-    
-    if __name__ == '__main__':
-    app.run(debug=True)"
+    return "Laboratório Pipeline Devops"
+
+if __name__ == '__main__':
+    app.run(debug=True)
